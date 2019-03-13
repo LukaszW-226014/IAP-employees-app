72ee0d4ad6a16d79afc0ded19fe7eb387f6fab17
Łukasz Wójcik <lukaszwojcik@ukaszs-MacBook-Air.local>
2019-03-13 09:00:06 +0100

    first commit

---
+++ src/main/java/com/lodz/pl/iap/employeesapp/EmployeesappApplication.java
@@ -0,0 +1,13 @@
+package com.lodz.pl.iap.employeesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesappApplication.class, args);
+    }
+
+}
