bf73595dd8fd0e77437dccac667eb427322cfe93
ElVi2 <48282124+ElVi2@users.noreply.github.com>
2019-04-03 09:45:15 +0200

    Added RestController

--- src/main/java/com/lodz/pl/iap/employeesapp/EmployeesappApplication.java
+++ src/main/java/com/lodz/pl/iap/employeesapp/EmployeesappApplication.java
@@ -2,13 +2,13 @@ package com.lodz.pl.iap.employeesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SpringBootApplication
 public class EmployeesappApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EmployeesappApplication.class, args);
-
     }
     public Integer myVar;
 }
